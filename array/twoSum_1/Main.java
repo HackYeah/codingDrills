f7594d676f6a65be64405a0144b27091282b02f7
Hackyeah <yermalitski@gmail.com>
2020-05-23 12:19:05 +0200

    Array.TwoSum_1

--- array/twoSum_1/Main.java
+++ array/twoSum_1/Main.java
@@ -1,8 +1,13 @@
 package codingDrills.array.twoSum_1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
+        int[] arr = {2, 7, 11, 15};
+        int[] result = s.twoSum(arr, 9);
+        System.out.println(Arrays.toString(result));
 
     }
 }
--- array/twoSum_1/Solution.java
+++ array/twoSum_1/Solution.java
@@ -1,4 +1,36 @@
 package codingDrills.array.twoSum_1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /* Not optimal solution
+        public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++){
+
+            int temp = target - nums[i];
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (temp == nums[j]){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return null;
+    }
+*/
 }
+
