e9c3502ce764ebf9105a7a6ea86e3944934a1aba
Hackyeah <yermalitski@gmail.com>
2020-05-23 14:59:22 +0200

    Array.MaxSubArray_53

--- array/maximumSubarray_53/Main.java
+++ array/maximumSubarray_53/Main.java
@@ -1,4 +1,10 @@
 package codingDrills.array.maximumSubarray_53;
 
 public class Main {
+    public static void main (String[] args){
+        int[] arr = {-2, -1, -3, -4, -1, -2, -1, -5, -4};
+        Solution s = new Solution();
+        int maxSubarraySum = s.maxSubArray(arr);
+        System.out.println("Maximum subarray summ is " + maxSubarraySum);
+    }
 }
--- array/maximumSubarray_53/Solution.java
+++ array/maximumSubarray_53/Solution.java
@@ -1,6 +1,73 @@
 package codingDrills.array.maximumSubarray_53;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null){
+            return 0;
+        }
+
+        int result = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int n : nums){
+            if (sum < 0) {
+                sum = n;
+            } else {
+                sum += n;
+            }
+            result = Math.max(result, sum);
+        }
+        return result;
+    }
+
+/*Not optional solution
+      public int maxSubArray(int[] nums) {
+        int max = nums[0];
+
+        for (int j = 0; j < nums.length; j++){
+            int sum = nums[j];
+
+            if (max < nums[j]){
+                max = nums[j];
+            }
+
+            for (int i = j+1; i < nums.length & j< nums.length; i++){
+                sum += nums[i];
+
+                if (max < sum){
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+*/
+
+/* Not optimal solution
+    public int maxSubArray(int[] nums) {
+        Map<int[], Integer> map = new HashMap<>();
+        for (int j = 0; j < nums.length; j++){
+            map.put(new int[] {nums[j]}, nums[j]);
+            int sum = nums[j];
+
+            for (int i = j+1; i < nums.length & j< nums.length; i++){
+                sum += nums[i];
+                map.put(Arrays.copyOfRange(nums,j,i+1), sum);
+            }
+        }
+
+        int max = Collections.max(map.values());
+
+        return max;
+
+    }
+*/
 }
 
 
