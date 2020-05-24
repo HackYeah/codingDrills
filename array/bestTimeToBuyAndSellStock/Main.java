bb2f37dbe17a17e845dd8f8a0b188898f078943d
Hackyeah <yermalitski@gmail.com>
2020-05-24 20:24:15 +0200

    Array.BestTimeToBuyAndSellStock_121

---
+++ array/bestTimeToBuyAndSellStock/Main.java
@@ -0,0 +1,9 @@
+package codingDrills.array.bestTimeToBuyAndSellStock;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {7, 1, 5, 3, 6, 4};
+        System.out.println("Profit: " + s.maxProfit(arr));
+    }
+}
---
+++ hashTable/containsDuplicate_217/Main.java
@@ -0,0 +1,9 @@
+package codingDrills.hashTable.containsDuplicate_217;
+
+public class Main {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println("Array contains duplicates: " + s.containsDuplicate(nums));
+    }
+}
