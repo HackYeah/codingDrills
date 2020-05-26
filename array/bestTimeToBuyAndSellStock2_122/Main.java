5853a5fdf5aa240169ce79d753d143325642c431
Hackyeah <yermalitski@gmail.com>
2020-05-26 13:09:25 +0200

    Array.BestTimeToBuyAndSellStock2_122

---
+++ array/bestTimeToBuyAndSellStock2_122/Main.java
@@ -0,0 +1,9 @@
+package codingDrills.array.bestTimeToBuyAndSellStock2_122;
+
+public class Main {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum profit: " + s.maxProfit(prices));
+    }
+}
